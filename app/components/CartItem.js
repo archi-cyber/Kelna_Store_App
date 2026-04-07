0ed07508bf96cdda0565a3d04bdccb38ade1e325
junioir <junior.tefo01gmail.com>
2026-04-07 13:15:49 -0400

    adding architecture

---
+++ app/components/CartItem.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+function CartItem(props) {
+    return (
+        <View style={styles.container}>
+            
+        </View>
+    );
+}
+
+export default CartItem;
+
+const styles = StyleSheet.create({
+    container: {
+        
+    }
+})
